175c812082bb3dcdcea888b42ad937d20dbbfb66
candlewoodtoby <johnsukoh@yahoo.com>
2015-08-14 17:18:58 -0700

    updated model associations for user & post

--- models/post.js
+++ models/post.js
@@ -6,7 +6,7 @@ module.exports = function(sequelize, DataTypes) {
     classMethods: {
       associate: function(models) {
         // associations can be defined here
-        // models.post.belongsTo(models.user)
+        models.post.belongsTo(models.user)
       }
     }
   });
