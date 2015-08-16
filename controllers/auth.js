813f263718f944483fdcaf5a0294790d9d42b40a
mustafa206 <mustafa.ahmed.007@gmail.com>
2015-08-16 11:37:55 -0700

    sign up & login

---
+++ controllers/auth.js
@@ -0,0 +1,75 @@
+var db = require('../models');
+var express = require('express');
+var router = express.Router();
+
+
+router.get('/login',function(req,res){
+    res.render('auth/login');
+});
+
+
+router.post('/login',function(req,res){
+
+  db.user.authenticate(req.body.email,req.body.password,function(err,user){
+    if(err){
+      res.send(err);
+    }else if(user){
+      req.session.user = user.id;
+      req.flash('success','You are logged in.');
+      res.redirect('/');
+    }else{
+      req.flash('danger','invalid username or password');
+      res.redirect('/auth/login');
+    }
+  });
+
+});
+
+router.get('/signup',function(req,res){
+    res.render('auth/signup');
+});
+
+
+router.post('/signup',function(req,res){
+  if(req.body.password != req.body.password2){
+    req.flash('danger','Passwords must match.')
+    res.redirect('/auth/signup');
+  }else{
+    db.user.findOrCreate({
+      where:{
+        email: req.body.email
+      },
+      defaults:{
+        email: req.body.email,
+        password: req.body.password,
+        name: req.body.name
+      }
+    }).spread(function(user,created){
+      if(created){
+        req.flash('success','You are signed up.')
+        res.redirect('/');
+      }else{
+        req.flash('danger','A user with that e-mail address already exists.');
+        res.redirect('/auth/signup');
+      }
+    }).catch(function(err){
+      if(err.message){
+        req.flash('danger',err.message);
+      }else{
+        req.flash('danger','unknown error.');
+        console.log(err);
+      }
+      res.redirect('/auth/signup');
+    })
+  }
+});
+
+
+router.get('/logout',function(req,res){
+  req.flash('info','You have been logged out.');
+  req.session.user = false;
+  res.redirect('/');
+});
+
+
+module.exports = router;
--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var session = require('express-session');
 var ejsLayouts = require('express-ejs-layouts')
+var session = require('express-session');
+var flash = require('connect-flash');
 
 //configuring express
 var app = express();
@@ -14,6 +16,36 @@ app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({extended:false}));
 //controllers
 app.use('/', require('./controllers/mainController.js'));
+app.use(session({
+  secret:'hdsvhioadfgnioadfgnoidfagoibna',
+  resave: false,
+  saveUninitialized: true
+}));
+
+app.use(flash());
+
+app.use(function(req,res,next){
+  if(req.session.user){
+    db.user.findById(req.session.user).then(function(user){
+      req.currentUser = user;
+      next();
+    });
+
+  }else{
+    req.currentUser = false;
+    next();
+  }
+});
+
+app.use(function(req,res,next){
+  res.locals.currentUser = req.currentUser;
+  res.locals.alerts = req.flash();
+  next();
+});
+
+app.use('/',require('./controllers/mainController.js'));
+app.use('/auth',require('./controllers/auth.js'));
+
 
 
 app.get("/", function(req, res){
