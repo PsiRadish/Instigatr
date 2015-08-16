8c81845c86f75ebc21b1753cc54a308a90b5b21d
PsiRadish <psiradish@comcast.net>
2015-08-16 13:08:56 -0700

    Auth fixed.

--- models/user.js
+++ models/user.js
@@ -19,10 +19,15 @@ module.exports = function(sequelize, DataTypes) {
         notEmpty: true
       }
     },
-    name: DataTypes.STRING
+    name: DataTypes.STRING,
+    score: { type: DataTypes.INTEGER, defaultValue: 0 }
   }, {
     classMethods: {
       associate: function(models) {
+        // associations can be defined here
+        models.user.hasMany(models.post);
+        models.user.belongsToMany(models.post, {through: "usersForPosts", as: "postsFor"});
+        models.user.belongsToMany(models.post, {through: "usersAgainstPosts", as: "postsAgainst"});
       },
       authenticate: function(email,password,callback){
         this.find({where:{email:email}}).then(function(user){
@@ -60,20 +65,20 @@ module.exports = function(sequelize, DataTypes) {
 
 
 
-  var user = sequelize.define('user', {
-    name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
-    score: { type: DataTypes.INTEGER, defaultValue: 0 }
-  }, {
-    classMethods: {
-      associate: function(models) {
-        // associations can be defined here
-        models.user.hasMany(models.post);
-        models.user.belongsToMany(models.post, {through: "usersForPosts", as: "postsFor"});
-        models.user.belongsToMany(models.post, {through: "usersAgainstPosts", as: "postsAgainst"});
-      }
-    }
-  });
-  return user;
-};
+//  var user = sequelize.define('user', {
+//    name: DataTypes.STRING,
+//    email: DataTypes.STRING,
+//    password: DataTypes.STRING,
+//    
+//  }, {
+//    classMethods: {
+//      associate: function(models) {
+//        // associations can be defined here
+//        models.user.hasMany(models.post);
+//        models.user.belongsToMany(models.post, {through: "usersForPosts", as: "postsFor"});
+//        models.user.belongsToMany(models.post, {through: "usersAgainstPosts", as: "postsAgainst"});
+//      }
+//    }
+//  });
+//  return user;
+//};
