095926f6a6bdfbacc89ed15fb6beddd016c6271c
Soren Smith <sorenrehkopfsmith@gmail.com>
2015-08-18 14:33:01 -0700

    random post function working

--- controllers/tagsController.js
+++ controllers/tagsController.js
@@ -2,7 +2,6 @@ var db = require('../models');
 var express = require('express');
 var router = express.Router();
 
-// --- POST SHOW
 router.get('/:id', function(req, res){
 
     db.tag.find({where:{id:req.params.id},include:[db.post]}).then(function(tag){
