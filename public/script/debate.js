bb63866126c241188bba08c5ef80b4daf62e036a
PsiRadish <KyleFiegener@gmail.com>
2016-02-05 14:44:48 -0800

    Fixed bad font names and breakdown of index layout at high resolutions.
    Fixed font stacks still containing underscored names "Roborto_condensed"
    and "Roboto_slab".  These fonts should now show up again.

--- public/script/debate.js
+++ public/script/debate.js
@@ -1,9 +1,9 @@
 $(function()
 {
-    if ($('#debate-page').length)
+    if ($('#debate-page').length) // check we're on a debate page
     {
         // console.log('debate-page');
-
+        
         $("#chat-output").mCustomScrollbar(
         {
             axis:"y", // vertical
@@ -17,9 +17,7 @@ $(function()
             scrollInertia: 1
         });
         
-        
-        
-        //up and downvote functionality
+        // up and downvote functionality
         $('#upVoteBtn').on('click',function(e){
             e.preventDefault();
             var id = $('#pId').val();
@@ -67,7 +65,7 @@ $(function()
                     imgOne = "<img src='http://graphics8.nytimes.com/"+news.responseJSON.response.docs[0].multimedia[0].url+"'>"
                 }
                 $('#newsListings').html('<div class="article-container"><div class="thumbNail all-20">'+imgOne+'</div><div class="article all-80"><p><a href="'+news.responseJSON.response.docs[0].web_url+'" target="_blank"><b>'+ news.responseJSON.response.docs[0].headline.main +'</b><br>'+news.responseJSON.response.docs[0].snippet+'</a></p></div></div>');
-
+                
                 for(i=1;i<8;i++){
                     var img = "<img src='http://placehold.it/40x50'>";
                     if(news.responseJSON.response.docs[i].multimedia[0]){
@@ -86,17 +84,17 @@ $(function()
             
             // console.log('viewHeight', viewHeight);
             // console.log('navHeight', navHeight);
-
+            
             var debatePage = $('#debate-page');
-
+            
             var postStuff = $('#post-stuff');
             var choices = $('#choices');
                 choices.height($('#headings-roll').outerHeight(true) + $('#wanna-join-for').outerHeight(true));
             var yourSide = $('#hows-my-arguing');
-
+            
             var chatOutput = $('#chat-output');
             var chatBox = $('#chat-box-container');
-
+            
             debatePage.height(viewHeight - navHeight);
             // console.log("debatePage.height()", debatePage.height());
             var contentHeight = debatePage.outerHeight();
@@ -120,7 +118,7 @@ $(function()
         sizeChat('init');
         // do it again on resize
         $(window).resize(sizeChat);
-
+        
         // var userData = $.ajax(
         // {
         //     url: '/userData',
@@ -128,45 +126,45 @@ $(function()
         // }).done(function(userData)
         // {
             // console.log("userData keys", Object.keys(userData));
-
+        
         var socket = io();
-
+        
         var postId = parseInt(window.location.pathname.split('/')[2]);
-
+        
         socket.emit('startChat', postId);
         socket.on('startChat_Response', function(userId, side, championFor, championAgainst)
         {
             console.log('Received startChat_Response', userId, side);
             // console.log('userId', userId);
-
+            
             $('#champ-for .author').html(championFor || '...');
             $('#champ-against .author').html(championAgainst || '...');
-
+            
             if (userId)
             {
                 // $('#choices').css('visibility', 'visible');
                 $('#must-be-logged-in').removeClass("disabled");
                 choiceShift(side);
-
+                
                 // CLIENT - CHOSE WHAT SIDE THEY ARE ON
                 $('#side-for .choose-side').on('click', function(e)
                 {
                     e.preventDefault();
                     $(this).blur();
-
+                    
                     socket.emit('choseSide', "for");
                 });
                 $('#side-against .choose-side').on('click', function(e)
                 {
                     e.preventDefault();
                     $(this).blur();
-
+                    
                     socket.emit('choseSide', "against");
                 });
                 $('.change-mind').on('click', function(e)
                 {
                     e.preventDefault();
-
+                    
                     socket.emit('choseSide', null);
                 });
                 // SERVER - CHOSE SIDE RESPONSE
@@ -174,7 +172,7 @@ $(function()
                 {
                     choiceShift(side);
                 });
-
+                
                 // CLIENT - WANT TO ENTER THE DEBATE
                 $('.enter-queue').on('click', function(e)
                 {
@@ -192,7 +190,7 @@ $(function()
                         $('.queued .ordinal').html(placeInLine);
                     }
                 });
-
+                
                 // SERVER - YOUR TURN
                 socket.on('becomeChampion', function(side)
                 {
@@ -208,24 +206,24 @@ $(function()
                 {   // TODO: An alert popin
                     disableChatBox('You have been kicked out of your position as speaker.');
                 });
-
+                
                 // // CLIENT - UP CONFIDENCE IN SPEAKER
                 // $('.vote-kudos').on('click', function(e)
                 // {
-
+                
                 // });
                 // // CLIENT - DOWN CONFIDENCE IN SPEAKER
                 // $('.vote-kick').on('click', function(e)
                 // {
-
+                
                 // });
-
+                
                 // CLIENT - COMMIT CHAT MESSAGE
                 ///////////////////////
                 // Block newline on Enter and send text to socket.io, unless Shift is held
                 ///////////////////////
                 var chatBox = $('#chat-box');
-
+                
                 chatBox.keydown(function(e)
                 {
                     e = e || event;  // IE compatibility thing of unknownn relevance
@@ -253,29 +251,29 @@ $(function()
                 disableChatBox("You are not logged in.");
                 $('#must-be-logged-in').addClass("disabled");
             }
-
+            
             // SERVER - CHAMP UPDATE
             socket.on('champUpdate', function(championFor, championAgainst)
             {
                 $('#champ-for .author').html(championFor || '...');
                 $('#champ-against .author').html(championAgainst || '...');
             });
-
+            
             // NEW CHAT MESSAGE
             socket.on('chatUpdate', function(authorName, content, side)
             {
                 // console.log('Update data:', update);
                 var chatOutput = $('#chat-output .mCSB_container');
-
+                
                 var newChatItem = $('<li class="new debate-message message-'+side+'">');
                 var h6 = $('<h6 class="author">').html(authorName);
                 newChatItem.append(h6);
                 newChatItem.append($('<span>'+content+'</span>'));
-
+                
                 // console.log(newChatItem);
-
+                
                 chatOutput.append(newChatItem);
-
+                
                 setTimeout(function() // wait to remove 'new' class so transition triggers
                 {
                     newChatItem.removeClass('new');
@@ -285,27 +283,27 @@ $(function()
                 }, 10);
             });
         });
-
+        
         function choiceShift(side)
         {
             $('.wanna-join .queued, .wanna-join .champion').addClass('hide-all');
             $('.wanna-join .not-queued').removeClass('hide-all');
-
+            
             if (side == 'for')
             {
                 $('#choices').addClass('side-chosen-for');
                 $('#choices').removeClass('side-chosen-against');
-
+                
                 // console.log("enablin'");
                 // enableChatBox();
                 disableChatBox("You must enter the queue to get your turn.");
-
+            
             }
             else if (side == 'against')
             {
                 $('#choices').addClass('side-chosen-against');
                 $('#choices').removeClass('side-chosen-for');
-
+                
                 // console.log("enablin'");
                 // enableChatBox();
                 disableChatBox("You must enter the queue to get your turn.");
@@ -313,7 +311,7 @@ $(function()
             else if (side == null)
             {
                 $('#choices').removeClass('side-chosen-for side-chosen-against');
-
+                
                 disableChatBox("You have not taken a side.");
             }
         }
